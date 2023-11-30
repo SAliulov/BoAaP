2859b3865b93f359cfe371ad205a7b674fd8d3d1
SAliulov <146759803+SAliulov@users.noreply.github.com>
2023-11-30 19:43:24 +0300

    Add files via upload
    Здравствуйте, в лабе 16.cpp задания 7-8 из лабы 18. Все текстовые файлы прилагаются

---
+++ Лабы 18-19/Лаба 16.cpp
@@ -0,0 +1,368 @@
+﻿// Лаба 16.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+//!!!!!!!!!!!!!!!!Сбросьте на рабочий стол, поменяйте имя пользователя (ctrl + H) на свое
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+#define NUM_ELEMENTS 100
+
+//ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ (И МАССИВЫ)
+
+int arr[NUM_ELEMENTS];
+
+int n = 0;
+
+
+//Вариант (Власенко)
+void printElements() {
+	printf("< ");
+	for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf(">\n");
+}
+
+//Задача 1.
+void Input_keyboard_array() {
+	printf("n = ");
+	scanf_s("%d", &n);
+	printf("input %d values: ", n);
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d", &arr[i]); 
+	}
+}
+
+
+void OddsX10() {
+	for (int i = 0; i < n; i++) {
+		if (arr[i] % 2 != 0) {
+			arr[i] = arr[i] * 10;
+		}
+	}
+}
+int findMin() {
+	int min = arr[0];
+	for (int i = 1; i < n; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+	}
+	return min;
+}
+
+//Задача 2. Кол-во элементов больше 10
+int Elements_more10() {
+	int count = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] > 10) {
+			count += 1;
+		}
+	}
+	return count;
+}
+//Задача 3. Последний чётный в 2 раза увеличить (у Власенко не учитываются отрицательные числа)
+int FindLastEven() {
+	int inx;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] % 2 == 0) {
+			inx = i;
+		}
+	}
+	return inx;
+}
+
+int Find_Min_Index() {
+	int index = 0;
+	int min = arr[0];
+	for (int i = 1; i < n; i++) {
+		if (arr[i] < min) {
+			index = i;
+			min = arr[i];
+		}
+	}
+	return index;
+}
+
+//Как с помощью Указателей (или еще как то) вернуть массив в Си???
+
+//int Find_First_And_Last_Even() {
+//	int index_first = 0;
+//	int index_last = n-1;
+//
+//	for (int i = 0; i < n; i++) {
+//		if (arr[i] % 2 == 0) {
+//			index_first = i;
+//			break;
+//		}
+//	}
+//
+//	for (int i = (n-1); i > 0; i--) {
+//		if (arr[i] % 2 == 0) {
+//			index_last = i;
+//			break;
+//		}
+//	}
+//	int Massive[2] = {index_first, index_last};
+//	return Massive;
+//}
+
+
+int Find_First_Even() {
+	int index_first = 101;
+
+	for (int i = 0; i < n; i++) {
+		if (arr[i] % 2 == 0) {
+			index_first = i;
+			break;
+		}
+	}
+	return index_first;
+}
+
+int Find_Last_Even() {
+	int index_last = 0;
+
+	for (int i = (n - 1); i > 0; i--) {
+		if (arr[i] % 2 == 0) {
+			index_last = i;
+			break;
+		}
+	}
+	return index_last;
+}
+
+//Лаба 17
+
+//Задание 1 Удалить элемент
+void deleteElement(int delIndex) {
+	for (int i = delIndex; i < n - 1; i++) {
+		arr[i] = arr[i + 1];
+	}
+	n--;
+}
+
+//Задание 2 Вставить элемент в i-ую позицию
+void insertElement(int Index, int value) {
+	for (int i = n; i > Index; i--) {
+		arr[i] = arr[i-1];
+	}
+	n++;
+	arr[Index] = value;
+}
+
+//Задание 3 Удалить минмимальный элемент
+
+void DeleteMinElement() {
+	int min_index = Find_Min_Index();
+	for (int i = min_index; i < n - 1; i++) {
+		arr[i] = arr[i + 1];
+	}
+	n--;
+}
+	
+//Задание 4 Перед минимальным элементом вставить 0
+void AddZeroBeforeMinElement(int indexMin, int value) {
+	for (int i = n; i > indexMin; i--) {
+		arr[i] = arr[i - 1];
+	}
+	n++;
+	arr[indexMin] = value;
+}
+
+void main() {
+	int item;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	//Задача 1. Базовые операции
+	
+	do {
+		printf("\n-----------------------------\n");
+		printf("Содержимое массива: ");
+		printElements();
+		printf("1: Ввести с клавиатуры массив\n");
+		printf("2: x10 для всех нечётных элементов\n");
+		printf("3: Найти минимальный элемент\n");
+		printf("4: Сколько elements > 10\n");
+		printf("5: Последний чётный элемент в массиве увеличить в 2 раза\n");
+		printf("6: Сколько чётных левее минимального\n");
+		printf("7: Увеличение в 10 раз для нечётных правее минимального\n");
+		printf("8: Все чётные элементы массива умножить на -1\n");
+		printf("9: Все элементы массива меньше 4 заменить на 4\n");
+		printf("10: Все элементы между первым четным и последним четным увеличить в 100 раз\n");
+		printf("11: Удалить заданный элемент\n");
+		printf("12: Вставить элемент перед i-ой позицией\n");
+		printf("13: Удалить минимальный элемент\n");
+		printf("14: Вставить 0 перед минимальным элементом\n");
+		printf("15: Загрузить массив из файла CurrentArr\n");
+
+		printf("\n0: Выйти из программы\n");
+
+		printf("Выбранная операция >>>>>>>>> ");
+
+		scanf_s("%d", &item);
+
+
+		
+
+
+		switch (item) {
+		case 1:
+			Input_keyboard_array();
+			break;
+		case 2:
+			OddsX10();
+			printElements();
+			break;
+		case 3: {
+				int min = findMin();
+				printf("min = %d \n", min);
+				break;
+			}
+		case 4: {
+				int counter = Elements_more10();
+				printf("Количество элементов больше 10: %d \n", counter);
+				break;
+			}
+		case 5: {
+				int index = FindLastEven();
+				if (index >= 0) {
+					arr[index] *= 2;
+				}
+				break;
+			}
+			
+		case 6: {
+				int Index_min = Find_Min_Index();
+				printf("Индекс минимального элемента = %d \n", Index_min);
+				int cnt = 0;
+
+				for (int i = 0; i < Index_min; i++) {
+					if (arr[i] % 2 == 0) {
+						cnt += 1;
+					}
+				}
+				printf("Левее минимального %d четных элементов \n", cnt);
+				break;
+			}
+
+		//Задача 5*
+		case 7: {
+				int Index_min = Find_Min_Index();
+				printf("Индекс минимального элемента = %d \n", Index_min);
+				
+				for (int i = (n-1); i > Index_min; i--) {
+					if (arr[i] % 2 != 0) {
+						arr[i] *= 10;
+					}	
+				}
+				break;
+			}
+		//Задача 7
+		case 8: {
+				for (int i = 0; i < n; i++) {
+					if (arr[i] % 2 == 0) {
+						arr[i] *= (-1);
+					}
+				}
+				break;
+			}
+		//Задача 8
+		case 9: {
+				for (int i = 0; i < n; i++) {
+					if (arr[i] < 4) {
+						arr[i] = 4;
+					}
+				}
+				break;
+			}
+		//Задача 10**
+		case 10: {
+			int first_index = Find_First_Even();
+			int last_index = Find_Last_Even();
+			if (first_index == 101) {
+				printf("Не могу совершить операцию, так как нет ни 1 чётного числа. Массив чисел остаётся прежним.");
+			}
+			else {
+				printf("Индекс первого четного элемента = %d \n", first_index);
+				printf("Индекс последнего чётного элемента = %d \n", last_index);
+			}
+			for (int i = 0; i > first_index; i--) {
+					arr[i] = arr[i] * 100;		
+				}	
+			}
+			break;
+
+		//Лаба 17
+		case 11: {
+				printf("Индекс удаляемого элемента = ");
+				int index;
+				scanf_s("%d", &index);
+				deleteElement(index);
+				
+			}
+			break;
+
+		case 12: {
+				printf("Перед каким элементом нужно вставить новый? Индекс = ");
+				int index;
+				scanf_s("%d", &index);
+				printf("Значение, которое нужно вставить: ");
+				int value;
+				scanf_s("%d", &value);
+				insertElement(index, value);
+
+			}
+			break;
+		case 13: {
+				int min = findMin();
+				printf("min = %d\n", min);
+				DeleteMinElement();
+
+			}
+			break;
+		case 14: {
+				int indexMin = Find_Min_Index();
+				printf("Индекс минимального элемента = %d\n", indexMin);
+				AddZeroBeforeMinElement(indexMin, 0);
+			}
+			break;
+
+		//Лаба 18
+		case 15: {
+				//В чем смысл загружать состояние массива из файла, в котором это же состояние массива до этого и сохранялась?
+				//Загрузка массива из файла (Лаба 18(8))
+				FILE* SavedArr = fopen("C:\\Users\\MIX PC\\Desktop\\CurrentArr.txt", "rt");
+				if (SavedArr == NULL) {
+					printf("ГГВП стоит задуматься Думайте.");
+					return;
+				}
+
+				for (int i8 = 0; i8 < n; i8++) {
+					fscanf(SavedArr, "%d ", &arr[i8]);
+				}
+				fclose(SavedArr);
+
+			}
+
+		}
+
+
+		//Сохранение состояния массива в файле (Лаба 18(7))
+		FILE* CurrentArr = fopen("C:\\Users\\MIX PC\\Desktop\\CurrentArr.txt", "wt");
+		if (CurrentArr == NULL) {
+			printf("ГГВП стоит задуматься Думайте.");
+			return;
+		}
+		for (int i7 = 0; i7 < n; i7++) {
+			fprintf(CurrentArr, "%d ", arr[i7]);
+		}
+		fclose(CurrentArr);
+		//Чек файл CurrentArr
+
+		
+
+	} while (item != 0);
+}
---
+++ Лабы 18-19/Лаба 18.cpp
@@ -0,0 +1,435 @@
+﻿// Лаба 18.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+//!!!!!!!!!!!!!!!!!!!Сбросьте на рабочий стол, поменяйте имя пользователя(MIX PC) (ctrl + H) на свое
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <Windows.h>
+#define MAXINDEX 10
+
+//Глобальные переменные из лабы 19
+int ARR[1000];
+int n1;
+//int *pARR;
+int Temperature[1000];
+int n3;
+int n4;
+int Finance[1000];
+int *finance;
+//Задача 1 и Задача 2 (комменты - задача 2(динамический массив))
+void Load() {
+    // Открытие входного файла
+    FILE* Filein1 = fopen("C:\\Users\\MIX PC\\Desktop\\in1(19).txt", "rt");
+    if (Filein1 == NULL) {
+        printf("Входной файл не найден\n");
+        return;
+    }
+
+    // Загрузка массива из входного файла
+    fscanf(Filein1, "%d", &n1);
+    //Выделение памяти под динамический массив
+
+    /*pARR = (int*)malloc(sizeof(int) * n1);*/
+
+    printf("Количество оценок ученика = %d\n", n1);
+    for (int i = 0; i < n1; i++) {
+        fscanf(Filein1, "%d", &ARR[i] /*&pARR[i]*/);
+    }
+    
+    // Закрытие входного файла
+    fclose(Filein1);
+    printf("Оценки: ");
+    for (int i = 0; i < n1; i++) {
+       printf("%d ", ARR[i]);
+    }
+}
+
+void SaveResult() {
+    // Вычисление среднего арифметического
+    float sa = 0;
+    float s = 0;
+    for (int i = 0; i < n1; i++) {
+        s += ARR[i];
+        /*s += pARR[i];*/
+    }
+    sa = s / n1;
+
+    // Выяснение, сколько элементов больше ср арифметического
+    int m = 0;
+    for (int i = 0; i < n1; i++) {
+        if (/*pARR[i]*/ ARR[i] > sa) {
+            m++;
+        }
+    }
+    FILE* Fileout6 = fopen("C:\\Users\\MIX PC\\Desktop\\out1(19).txt", "wt");
+    if (Fileout6 == NULL) {
+        printf("Выходной файл не найден\n");
+        return;
+    }
+
+    // Сохранение элементов больших ср арифметического
+    fprintf(Fileout6, "Среднее арифметическое = %d\nОценки выше ср арифм: ", m);
+    for (int i = 0; i < n1; i++) {
+        if (/*pARR[i]*/ ARR[i] > sa) {
+            fprintf(Fileout6, "%d ", ARR[i] /*pARR[i]*/);
+        }
+    }
+    // Закрытие файла
+    fclose(Fileout6);
+}
+
+int Max() {
+    int max = finance[0];
+    for (int i = 1; i < n4; i++) {
+        if (finance[i] > max) {
+            max = finance[i];
+        }
+    }
+    return max;
+}
+
+void main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    //Задача 1 из консоли
+    printf("ВласенкО Олег (хорошая работа)\nЗадача 1\n");
+    printf("Введите 3 числа в консоль, для их перемножения: ");
+
+    // Числа, читаемые из входного файла
+    int a, b, c;
+    scanf_s("%d%d%d", &a, &b, &c);
+    // Произведение, выводимое в выходной файл
+    int S;
+    printf("ввели: %d, %d, %d\n", a, b, c);
+    // Обработка
+    S = a * b * c;
+    printf("S = %d\n", S);
+
+
+
+
+    //Задача 2 из файла
+
+    printf("\nВласенкО Олег\nЗадача 2\n");
+    // Числа, читаемые из входного файла
+    int a1, b1, c1;
+    // Произведение, выводимое в выходной файл
+    int p;
+
+    // Входной файл
+    FILE* fin = fopen("C:\\Users\\MIX PC\\Desktop\\in2.txt.txt", "rt");
+    if (fin == NULL) {
+        printf("Входной файл не найден");
+        return;
+    }
+    fscanf(fin, "%d%d%d", &a1, &b1, &c1);
+    fclose(fin);
+    // логирование ввода
+    printf("ввели: %d, %d, %d\n", a1, b1, c1);
+
+    // Обработка
+    p = a1 * b1 * c1;
+
+    // логирование вывода
+    printf("p =  %d", p);
+
+    // Запись в выходной файл
+
+    // Выходной файл
+    FILE* fout;
+    fout = fopen("C:\\Users\\MIX PC\\Desktop\\out2.txt.txt", "wt");
+    if (fout == NULL) {
+        printf("Выходной файл не создался");
+        return;
+    }
+    fprintf(fout, "%d", p);
+    fclose(fout);
+    printf("\nЧек файл out2\n\n");
+
+
+    //Задача 3
+    printf("\nВласенкО Олег\nЗадача 3\n");
+    int q, w, e, r, t;
+    int Sum;
+    printf("ВВедите 5 целых числа: \n");
+    scanf_s("%d%d%d%d%d", &q, &w, &e, &r, &t);
+    Sum = q + w + e + r + t;
+    printf("Введенные числа: %d, %d, %d, %d, %d\nСумма = %d\n", q, w, e, r, t, Sum);
+
+
+
+    //Задача 4
+    printf("\nВласенкО Олег\nЗадача 4\n");
+
+    int q1, q2, q3, q4, q5;
+    int Summa;
+    FILE* filein = fopen("C:\\Users\\MIX PC\\Desktop\\in4.txt", "rt");
+    if (filein == NULL) {
+        printf("Выходной файл не создался");
+        return;
+    }
+    fscanf(filein, "%d%d%d%d%d", &q1, &q2, &q3, &q4, &q5);
+    fclose(filein);
+    Summa = q1 + q2 + q3 + q4 + q5;
+
+    printf("ввели: %d, %d, %d, %d, %d\n", q1, q2, q3, q4, q5);
+
+    FILE* fileout = fopen("C:\\Users\\MIX PC\\Desktop\\out4.txt", "wt");
+    if (fileout == NULL) {
+        printf("Не найден файл out4");
+        return;
+    }
+    fprintf(fileout, "%d + %d + %d + %d + %d = %d\n", q1, q2, q3, q4, q5, Summa);
+    fclose(fileout);
+    printf("\nЧек файл out4\n\n");
+
+
+    //Задача 5 Обработка одномерного массива
+    printf("\nВласенкО Олег\nЗадача 5\n");
+    int array[MAXINDEX];
+    int firstr;
+    int i;
+
+    FILE* Filein = fopen("C:\\Users\\MIX PC\\Desktop\\in5.txt", "rt");
+    if (Filein == NULL) {
+        printf("Не найден");
+        return;
+    }
+
+    fscanf(Filein, "%d", &firstr);
+
+    for (i = 0; i < firstr; i++) {
+        fscanf(Filein, "%d", &array[i]);
+    }
+
+    fclose(Filein);
+    
+    printf("Ввели массив array[%d] = [ ", firstr);
+    for (i = 0; i < firstr; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("]");
+    int Average = 0;
+    for (i = 0; i < firstr; i++) {
+        Average += array[i];
+    }
+    float Summarr = float(Average)/firstr;
+    printf("\nAVERAGE ARITHMETIC = %f", Summarr);
+
+    for (i = 0; i < firstr; i++) {
+        if (array[i] > Summarr) {
+            array[i] *= 10;
+        }
+    }
+    printf("\nВывели массив array[%d] = [ ", firstr);
+    for (i = 0; i < firstr; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("]");
+    printf("\nЧек файл out5\n\n");
+
+    //Запись
+    FILE* Fileout = fopen("C:\\Users\\MIX PC\\Desktop\\out5.txt", "wt");
+    if (Fileout == NULL) {
+        printf("Didnot exist");
+        return;
+    }
+
+    for (i = 0; i < firstr; i++) {
+        fprintf(Fileout, "%d ", array[i]);
+    }
+    
+    fclose(Fileout);
+
+
+
+    //Задача 6
+    printf("\nВласенкО Олег\nЗадача 6\n");
+    FILE *Filein6 = fopen("C:\\Users\\MIX PC\\Desktop\\in6.txt", "rt");
+    int n;
+    int Arr6[MAXINDEX];
+    int i6;
+    if (Filein6 == NULL) {
+        printf("Не найден");
+        return;
+    }
+
+    fscanf(Filein6, "%d", &n);
+
+    for (i6 = 0; i6 < n; i6++) {
+        fscanf(Filein6, "%d", &Arr6[i6]);
+    }
+
+    fclose(Filein6);
+
+    printf("Ввели массив Arr6[%d] = [ ", n);
+    for (i6 = 0; i6 < n; i6++) {
+        printf("%d ", Arr6[i6]);
+    }
+    printf("]");
+
+    int Average6 = 0;
+    for (i6 = 0; i6 < n; i6++) {
+        Average6 += Arr6[i6];
+    }
+    float SummArr6 = float(Average6) / n;
+    printf("\nAVERAGE ARITHMETIC = %f", SummArr6);
+
+    for (i6 = 0; i6 < n; i6++) {
+        if (Arr6[i6] % 2 == 0 and Arr6[i6] < SummArr6) {
+            Arr6[i6] /= 2;
+        }
+    }
+    printf("\nВывели массив Arr6[%d] = [ ", n);
+    for (i6 = 0; i6 < n; i6++) {
+        printf("%d ", Arr6[i6]);
+    }
+    printf("]");
+
+    FILE* Fileout6 = fopen("C:\\Users\\MIX PC\\Desktop\\out6.txt", "wt");
+    if (Filein6 == NULL) {
+        printf("Не создан");
+        return;
+    }
+
+    for (i6 = 0; i6 < n; i6++) {
+        fprintf(Fileout6, "%d ", Arr6[i6]);
+    }
+    printf("\nЧек файл out6\n\n");
+    fclose(Fileout6);
+    printf("\n\n\n\n");
+
+    //Мои так называемые задачи 7-8 - см. файл лаба16.cpp (case15 - 334 строка и 352 строка)
+
+    //Лаба 19 Динамические массивы.
+    
+    printf("\nЛабораторная №19. ВласенкО Олег\nЗадача 1-2\n");
+    
+    Load();
+    SaveResult();
+
+    /*free(pARR);*/
+    printf("\nЧек файл out1(19)\n\n");
+    
+    printf("\nВласенкО Олег\nЗадача 3\n");
+
+
+    FILE* Tempin = fopen("C:\\Users\\MIX PC\\Desktop\\in3(19).txt", "rt");
+    if (Tempin == NULL) {
+        printf("Не удается открыть файл");
+        return;
+    }
+    fscanf(Tempin, "%d", &n3);
+    printf("Количество измерений = %d\n", n3);
+
+    for (int i = 0; i < n3; i++) {
+        fscanf(Tempin, "%d ", &Temperature[i]);
+
+    }
+    fclose(Tempin);
+    printf("Ввели массив Temperature[%d] = [ ", n3);
+    for (int i = 0; i < n3; i++) {
+        printf("%d ", Temperature[i]);
+    }
+    printf("]");
+    int Sumtemp = 0;
+    for (int i = 0; i < n3; i++) {
+        Sumtemp += Temperature[i];
+    }
+
+    double AverageTemp = double(Sumtemp) / n3;
+    
+    FILE *Tempout = fopen("C:\\Users\\MIX PC\\Desktop\\out3(19).txt", "wt");
+    if (Tempout == NULL) {
+        printf("Не удалось создать файл");
+        return;
+    }
+    //Почему на выводе среднего арифмeтического выводятся мусорные значения, но алгоритм правильно выводит в файл элементы меньшие среднего арифм?
+    printf("\nСреднее аримфетическое = %d\n", AverageTemp);
+    for (int i = 0; i < n3; i++) {
+        if (Temperature[i] > 0 and Temperature[i] < AverageTemp) {
+            fprintf(Tempout, "%d ", Temperature[i]);
+        }
+    }
+    printf("\nЧек файл out3(19)\n\n");
+
+
+
+
+
+
+
+
+    //Задача 4
+    
+    printf("\nВласенкО Олег\nЗадача 4\n");
+    // Открытие входного файла
+    FILE* Filein4 = fopen("C:\\Users\\MIX PC\\Desktop\\in4(19).txt", "rt");
+    if (Filein4 == NULL) {
+        printf("Входной файл не найден\n");
+        return;
+    }
+
+    // Загрузка массива из входного файла
+    fscanf(Filein4, "%d\n", &n4);
+    //Выделение памяти под динамический массив
+
+    finance = (int*)malloc(sizeof(int) * n4);
+
+    printf("Количество доходов = %d\n", n4);
+    for (int i = 0; i < n4; i++) {
+        fscanf(Filein4, "%d", &finance[i]);
+    }
+
+    // Закрытие входного файла
+    fclose(Filein4);
+    printf("Доходы: ");
+    for (int i = 0; i < n4; i++) {
+        printf("%d ", finance[i]);
+    }
+
+    
+    // ненавижу флоат, поэтому инт
+    int m1 = 0;
+    int Maximum = Max();
+    int Ifmax0 = Maximum * 2;
+    int Ifmax = Ifmax0 / 3;
+    for (int i = 0; i < n4; i++) {
+        if (finance[i] >= Ifmax) {
+            m1++;
+        }
+    }
+
+    FILE* Fileout4 = fopen("C:\\Users\\MIX PC\\Desktop\\out4(19).txt", "wt");
+    if (Fileout4 == NULL) {
+        printf("Выходной файл не найден\n");
+        return;
+    }
+
+
+    fprintf(Fileout4, "Количество доходов, составляющих как минимум 2/3 от максимального дохода = %d\n", m1);
+    
+
+   
+    
+    // Закрытие файла
+    printf("\n2/3 от максимального элемента = %d\n", Ifmax);
+    for (int i = 0; i < n4; i++) {
+        if (finance[i] >= Ifmax) {
+            fprintf(Fileout4, "%d ", finance[i]);
+        }
+    }
+    free(finance);
+    fclose(Fileout4);
+
+
+    
+    printf("\nЧек файл out4(19)\n\n");
+
+}
+
